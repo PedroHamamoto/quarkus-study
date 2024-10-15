bafc69adc04c5a65aeb30b21ed2cabddd3434a39
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-10-14 18:43:11 -0700

    configuring liquibase

---
+++ src/main/java/com/hamamoto/quarkus_study/entity/Author.java
@@ -0,0 +1,26 @@
+package com.hamamoto.quarkus_study.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private String name;
+}
